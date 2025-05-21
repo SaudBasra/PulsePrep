6deb9748ea9f81b53d44f13c4b88d808c1d8ef0b
SaudBasra <117218133+SaudBasra@users.noreply.github.com>
2025-05-21 20:12:01 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*gm9ehhq^vj5spmcam1!+ubt$9x+if3!peec!qg@8!ymn=r*y0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['194.31.53.177']
+ALLOWED_HOSTS = ['194.31.53.177', 'pulseprep.net', 'www.pulseprep.net', '127.0.0.1', 'localhost']
 
 # Application definition
 
